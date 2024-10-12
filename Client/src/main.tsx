0df19dd30fc2efa7947cca34bed49f18468803c7
Mohammad Khalilzadeh <muhammadkhalilzadeh1998@gmail.com>
2024-10-12 13:12:23 +0330

    fe dependencies

--- Client/src/main.tsx
+++ Client/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+import "@fontsource/inter";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
