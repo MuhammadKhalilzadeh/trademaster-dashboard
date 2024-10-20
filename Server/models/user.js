21f741fe09ecdcf8c5e362bdec8dc443a9a2ff0e
Mohammad Khalilzadeh <muhammadkhalilzadeh1998@gmail.com>
2024-10-20 19:41:16 +0330

    developments

---
+++ Server/models/user.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+  firstname: {
+    type: String,
+    required: true,
+  },
+  lastname: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+});
+
+export default mongoose.model("User", UserSchema);
